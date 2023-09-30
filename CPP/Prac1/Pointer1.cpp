24ec06754a238e87c3a620bd2f72379b722f0c2a
Shubham <87639594+webtdeveloper@users.noreply.github.com>
2023-09-30 07:13:53 +0000

    commit

---
+++ CPP/Prac1/Pointer1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+
+    // char ch = 'A';
+    // char* message {&ch};
+    // std::cout<< "Address : " << &message << " Value : " << *message << "\n";
+    // std::cout<< "Address : " << message << "\n";
+
+
+    // It give compiler error
+    // char* message = {"Hello Shubham!"};
+    // std::cout<< "Address : " << &message << " Value : " << *message << "\n";
+    // std::cout<< "Message : " << message << "\n";
+
+
+    // const char* message = {"Hello Shubham!"};
+    // std::cout<< "Address : " << &message << " Value : " << *message << "\n";
+    // std::cout<< "Message : " << message << "\n";
+
+    
+
+
+}
