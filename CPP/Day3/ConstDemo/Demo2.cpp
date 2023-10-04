d48c61f4f0e6e0d354281eed943b460f7ed03022
Shubham <87639594+webtdeveloper@users.noreply.github.com>
2023-10-04 15:42:29 +0000

    commit

--- CPP/Day3/ConstDemo/Demo2.cpp
+++ CPP/Day3/ConstDemo/Demo2.cpp
@@ -5,15 +5,25 @@ void Demo(){
     int n2 = 20;
     //const is applied on 'int' not on 'int*'
     const int* ptr = &n1;
+    std::cout<<ptr<<"\n";
+    std::cout<<*ptr<<"\n";
 
     //since pointer itself is not constant, its 'constant' can be changed,
     //so address inside ptr can be changed
-    const int * ptr = &n1;
+    ptr = &n2;
+    std::cout<<ptr<<"\n";
+    std::cout<<*ptr<<"\n";
 
     //since int part is constant, I cannot use pointer to change n1's value
-    //*ptr = 100; //not allowed
+    // *ptr = 100; //not allowed
+    // std::cout<<ptr<<"\n";
+    // std::cout<<*ptr<<"\n";
 
 
-    const int* ptr = &n1;
+    //const int* ptr = &n1;
 
+}
+
+int main(){
+    Demo();
 }
--- CPP/Day3/ConstDemo/Demo3.cpp
+++ CPP/Day3/ConstDemo/Demo3.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 
-void Demo(){
+void Demo3(){
     int n1 = 10;
     int n2 = 20;
-    //const is applied on 'int' not on 'int*'
+    //const is applied on 'int*' not on 'int'
     int const * ptr = &n1;
+    std::cout<<ptr<<"\n";
+    std::cout<<*ptr<<"\n";
 
     //since pointer itself is not constant, its 'constant' can be changed,
     //so address inside ptr can be changed
-    const int * ptr = &n1;
+    // ptr = &n2;
+    // std::cout<<ptr<<"\n";
+    // std::cout<<*ptr<<"\n";
 
     //since int part is constant, I cannot use pointer to change n1's value
-    //*ptr = 100; //not allowed
+    // *ptr = 100; //not allowed
+    // std::cout<<ptr<<"\n";
+    // std::cout<<*ptr<<"\n";
 
 
     const int* ptr = &n1;
 
+}
+
+int main(){
+    Demo3();
 }
